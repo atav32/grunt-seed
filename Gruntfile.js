b8587fa499036c2af6030f32419da0f73cd9a815
Brian Zhang <tangerinewhite32@gmail.com>
2014-11-02 21:44:48 -0800

    setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,6 @@ module.exports = function(grunt) {
   var path = require('path');
   require('load-grunt-config')(grunt, {
     configPath: path.join(process.cwd(), 'grunt_modules'),
-    config: 'package.json',
-    scope: ['devDependencies', 'dependencies']
   });
 
 };
